ded91ffad9a284889a330fab3b4e35906926f4b7
Lars Behrenberg <l.behrenberg@gmail.com>
2020-01-16 11:45:13 +0900

    change about button

--- src/homepage/components/about.js
+++ src/homepage/components/about.js
@@ -18,7 +18,7 @@ export default () => (
       <div className="bg-gray flex flex-column justify-center items-center pa2 pv5">
         <h1 className="fw1 display db dark-gray f2 tc white">{data.site.siteMetadata.homepageHeader}</h1>
         <p className="f4 serif mw7 tc lh-copy white">{data.site.siteMetadata.homepageAbout}</p>
-        <Link to="/about" className="mt3 db no-underline ph5 pv3 sans-serif near-white bg-dark-gray ttu tracked b hover-bg-mid-gray">About Tyra</Link>
+        <Link to="/about" className="mt3 db no-underline ph5 pv3 sans-serif near-white bg-dark-gray ttu tracked b hover-bg-mid-gray">About us</Link>
       </div>
     )} />
 )
